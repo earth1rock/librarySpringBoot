a3d17ec580fca53c9ff17c6d7fffed94ad2efc6b
Alexander Vasiliev <smash3243@gmail.com>
2023-06-29 12:52:37 +0300

    init

---
+++ src/main/java/com/example/libarySpringBoot/LibarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.libarySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibarySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibarySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/libarySpringBoot/LibarySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libarySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibarySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
